a6596b169ed763db4a4c8cb545c7431594f9a558
simon.sjoqvist <321750073+ntijoh-simon-sjoqvist@users.noreply.github.com>
2026-09-09 10:48:45 +0200

    all

--- lib/pluggy/models/fruit.ex
+++ lib/pluggy/models/fruit.ex
@@ -4,12 +4,12 @@ defmodule Pluggy.Fruit do
   alias Pluggy.Fruit
 
   def all do
-    Postgrex.query!(DB, "SELECT * FROM fruits", []).rows
+    Postgrex.query!(DB, "SELECT * FROM pizza", []).rows
     |> to_struct_list
   end
 
   def get(id) do
-    Postgrex.query!(DB, "SELECT * FROM fruits WHERE id = $1 LIMIT 1", [String.to_integer(id)]).rows
+    Postgrex.query!(DB, "SELECT * FROM pizza WHERE id = $1 LIMIT 1", [String.to_integer(id)]).rows
     |> to_struct
   end
 
@@ -20,7 +20,7 @@ defmodule Pluggy.Fruit do
 
     Postgrex.query!(
       DB,
-      "UPDATE fruits SET name = $1, tastiness = $2 WHERE id = $3",
+      "UPDATE pizza SET name = $1, tastiness = $2 WHERE id = $3",
       [name, tastiness, id]
     )
   end
@@ -29,11 +29,11 @@ defmodule Pluggy.Fruit do
     name = params["name"]
     tastiness = String.to_integer(params["tastiness"])
 
-    Postgrex.query!(DB, "INSERT INTO fruits (name, tastiness) VALUES ($1, $2)", [name, tastiness])
+    Postgrex.query!(DB, "INSERT INTO pizza (name, tastiness) VALUES ($1, $2)", [name, tastiness])
   end
 
   def delete(id) do
-    Postgrex.query!(DB, "DELETE FROM fruits WHERE id = $1", [String.to_integer(id)])
+    Postgrex.query!(DB, "DELETE FROM pizza WHERE id = $1", [String.to_integer(id)])
   end
 
   def to_struct([[id, name, tastiness]]) do
