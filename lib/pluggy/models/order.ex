5f4831721a7ff146f950987f8f846d8c55664bb7
ntijoh-adam-clarke <adam.clarke@elev.ga.ntig.se>
2026-09-14 20:52:41 +0200

    changes

--- lib/pluggy/models/order.ex
+++ lib/pluggy/models/order.ex
@@ -41,4 +41,9 @@ defmodule Pluggy.Order do
     end)
   end
 
+  def update(pizza_id) do
+    Postgrex.query!(DB,"INSERT INTO orders(status) VALUES ($1)", [])
+    Postgrex.query!(DB, "INSERT INTO order_items(order_id, pizza_id) VALUES ($1, $2)", [1, pizza_id])
+  end
+
 end
